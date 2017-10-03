64e3fdee5099ec112bcd09a74d9b9e05c4800d55
Left94 <leftheri94@gmail.com>
2017-10-03 13:08:32 +0200

    add Hammer weapon class

---
+++ P-3/Hammer.swift
@@ -0,0 +1,27 @@
+//
+//  Hammer.swift
+//  P-3
+//
+//  Created by vincent  on 03/10/2017.
+//  Copyright © 2017 vincent . All rights reserved.
+//
+
+import Foundation
+
+
+
+
+class Hammer : Weapon {
+    
+    override init () {
+        super.init()
+        
+        
+        damagePoints = 50
+        type = WeaponType.DamagingWeapon
+        
+        
+    }
+    
+    
+}
